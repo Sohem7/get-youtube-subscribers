a5df0389bd9a8cec17285abc5a706297ea4d76aa
Sohem7 <sohemrajanijam@gmail.com>
2023-08-04 12:53:16 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -101,6 +101,10 @@ app.get('/subscribers/:id', async (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
